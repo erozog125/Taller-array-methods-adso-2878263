8257090e18c5d264883e4012fdb2410c059e5800
angiemortizs <angiemortizs@gmail.com>
2024-08-15 20:49:55 -0500

    Color activity working, arrays missing

--- Page1/Page1/app.js
+++ Page1/Page1/app.js
@@ -1,1 +1,8 @@
-const selector = document.getElementById ('backgroundSelector')
+let colorSelect = document.getElementById("backgroundSelector");
+
+colorSelect.addEventListener("change", function() {
+
+    let selectedColor = colorSelect.value;
+
+    document.body.style.backgroundColor = selectedColor;
+});
---
+++ Page2/app.js
@@ -0,0 +1,17 @@
+const frutas = [
+    { nombre: "Manzana", imagen: 'assets/manzana.png' },
+    { nombre: "Banana", imagen: "https://via.placeholder.com/150?text=Banana" },
+    { nombre: "Cereza", imagen: "https://via.placeholder.com/150?text=Cereza" },
+    { nombre: "Fresa", imagen: "https://via.placeholder.com/150?text=Fresa" },
+    { nombre: "Limón", imagen: "https://via.placeholder.com/150?text=Limón" },
+    { nombre: "Mango", imagen: "https://via.placeholder.com/150?text=Mango" },
+    { nombre: "Naranja", imagen: "https://via.placeholder.com/150?text=Naranja" },
+    { nombre: "Papaya", imagen: "https://via.placeholder.com/150?text=Papaya" },
+    { nombre: "Pera", imagen: "https://via.placeholder.com/150?text=Pera" },
+    { nombre: "Piña", imagen: "https://via.placeholder.com/150?text=Piña" },
+    { nombre: "Sandía", imagen: "https://via.placeholder.com/150?text=Sandía" },
+    { nombre: "Uva", imagen: "https://via.placeholder.com/150?text=Uva" },
+    { nombre: "Durazno", imagen: "https://via.placeholder.com/150?text=Durazno" },
+    { nombre: "Coco", imagen: "https://via.placeholder.com/150?text=Coco" },
+    { nombre: "Melón", imagen: "https://via.placeholder.com/150?text=Melón" }
+];
