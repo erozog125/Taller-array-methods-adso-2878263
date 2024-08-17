05517af9f27231f728c9c6c13ddb4de200af0b75
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-08-17 11:07:08 -0500

    Modificacion ventana inicial

--- practice-1/app.js
+++ practice-1/app.js
@@ -1,5 +1,6 @@
 const colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple'];
 const colorSelect = document.getElementById('color-select');
+
 colors.forEach(color => {
     const option = document.createElement('option');
     option.textContent = color;
@@ -8,8 +9,6 @@ colors.forEach(color => {
 
 colorSelect.addEventListener('change', function() {
     const selectedColor = colorSelect.value;
-
     document.body.style.backgroundColor = selectedColor;
-
 });
 
