26a9ed2a1cd9db96b0f4762219aa02a69b6f5724
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-17 09:27:18 -0500

    creacion array frutas

---
+++ buscador/js/fruit.js
@@ -0,0 +1,78 @@
+export const fruits = [
+  const frutas = [
+    {
+        id: 1,
+        nombre: "Manzana",
+        image: "/buscador/assets/images/manzana.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Banana",
+        image: "/buscador/assets/images/banano.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Naranja",
+        image: "/buscador/assets/images/naranja.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Pera",
+        image: "/buscador/assets/images/pera.jpg"
+    },
+    {
+        id: 5,
+        nombre: "Fresa",
+        image: "/buscador/assets/images/fresa.jpg"
+    },
+    {
+        id: 6,
+        nombre: "Uva",
+        image: "/buscador/assets/images/pexels-luiz-m-santos-uva.jpg"
+    },
+    {
+        id: 7,
+        nombre: "Mango",
+        image: "/buscador/assets/images/mango.jpg"
+    },
+    {
+        id: 8,
+        nombre: "Piña",
+        image: "/buscador/assets/images/piña.jpg"
+    },
+    {
+        id: 9,
+        nombre: "Kiwi",
+        image: "/buscador/assets/images/kiwi.jpg"
+    },
+    {
+        id: 10,
+        nombre: "Melón",
+        image: "/buscador/assets/images/melon.jpg"
+    },
+    {
+        id: 11,
+        nombre: "Sandía",
+        image: "/buscador/assets/images/sandia.jpg"
+    },
+    {
+        id: 12,
+        nombre: "Papaya",
+        image: "/buscador/assets/images/papaya.jpg"
+    },
+    {
+        id: 13,
+        nombre: "Guayaba",
+        image: "/buscador/assets/images/guayaba.jpg"
+    },
+    {
+        id: 14,
+        nombre: "Durazno",
+        image: "/buscador/assets/images/durazno.jpg"
+    },
+    {
+        id: 15,
+        nombre: "Aguacate",
+        image: "/buscador/assets/images/aguacate.jpg"
+    }
+];
