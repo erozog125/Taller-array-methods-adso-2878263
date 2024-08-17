a15f8bfe68e36a810a8b4856e800578217d39a9a
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-17 12:01:16 -0500

    creando card

--- buscador/js/buscador.js
+++ buscador/js/buscador.js
@@ -1,7 +1,27 @@
 import { fruits } from "./fruit.js"
 
+const container = document.getElementById('section-fruit')
 let searchfruit = document.getElementById('search')
 searchfruit.addEventListener('change', function (){
     const resultSearch = fruits.find(fruit => fruit.nombre.toLowerCase() == searchfruit.value)
-    console.log(resultSearch.nombre)
+    
+    createFruitCard(resultSearch)
 })
+
+function createFruitCard(resultSearch){
+    const divCard = document.createElement("div")
+    divCard.classList.add("result-Search")
+    divCard.id= resultSearch.id
+
+    const imgContent = document.createElement("img")
+    imgContent.alt = resultSearch.nombre
+    imgContent.src = resultSearch.image
+
+    const nameFruit = document.createElement("h2")
+    nameFruit.textContent = resultSearch.nombre
+
+    divCard.appendChild(imgContent)
+    divCard.appendChild(nameFruit)
+    container.appendChild(divCard)
+
+}
