e4623a91202b8949e2270f5daaadf212c2e1b536
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-17 14:27:55 -0500

    c

--- buscador/js/buscador.js
+++ buscador/js/buscador.js
@@ -1,27 +1,45 @@
-import { fruits } from "./fruit.js"
+import { fruits } from "./fruit.js";
 
-const container = document.getElementById('section-fruit')
-let searchfruit = document.getElementById('search')
-searchfruit.addEventListener('change', function (){
-    const resultSearch = fruits.find(fruit => fruit.nombre.toLowerCase() == searchfruit.value)
-    
-    createFruitCard(resultSearch)
-})
+const container = document.getElementById("section-fruit");
+let searchfruit = document.getElementById("search");
+let countChange = 0;
+let imgContent;
+let nameFruit;
+let divCard;
+searchfruit.addEventListener("change", function () {
+  countChange++;
+  const resultSearch = fruits.find(
+    (fruit) => fruit.nombre.toLowerCase() == searchfruit.value
+  );
 
-function createFruitCard(resultSearch){
-    const divCard = document.createElement("div")
-    divCard.classList.add("result-Search")
-    divCard.id= resultSearch.id
+  createFruitCard(resultSearch);
+});
 
-    const imgContent = document.createElement("img")
-    imgContent.alt = resultSearch.nombre
-    imgContent.src = resultSearch.image
+function createFruitCard(resultSearch) {
+  if (countChange < 2) {
+    divCard = document.createElement("div");
+    divCard.classList.add("result-Search");
+    divCard.id = resultSearch.id;
 
-    const nameFruit = document.createElement("h2")
-    nameFruit.textContent = resultSearch.nombre
+    imgContent = document.createElement("img");
+    imgContent.alt = resultSearch.nombre;
+    imgContent.src = resultSearch.image;
 
-    divCard.appendChild(imgContent)
-    divCard.appendChild(nameFruit)
-    container.appendChild(divCard)
+    nameFruit = document.createElement("h2");
+    nameFruit.textContent = resultSearch.nombre;
 
+    divCard.appendChild(imgContent);
+    divCard.appendChild(nameFruit);
+    container.appendChild(divCard);
+  } else {
+    updateFruit(resultSearch);
+  }
+
+  function updateFruit(resultSearch) {
+    let cardFruit = document.getElementById(resultSearch.id);
+
+    cardFruit.querySelector("img").alt = resultSearch.nombre;
+    cardFruit.querySelector("img").alt = resultSearch.image;
+    cardFruit.querySelector("h2").textContent = resultSearch.nombre;
+  }
 }
